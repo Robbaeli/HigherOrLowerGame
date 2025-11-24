580b1f1884680b1652925e38255b66e3f1679a22
Robbaeli <robineliasson@live.se>
2025-11-24 21:04:02 +0100

    Visual improvements + Gameactivity.kt

--- app/src/main/java/com/robbaflow/higherorlowergame/GameActivity.kt
+++ app/src/main/java/com/robbaflow/higherorlowergame/GameActivity.kt
@@ -1,20 +1,44 @@
 package com.robbaflow.higherorlowergame
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.color.utilities.Score
 
 class GameActivity : AppCompatActivity() {
+
+    private lateinit var tvScore: TextView
+    private lateinit var tvCardsLeft: TextView
+    private lateinit var tvCard: TextView
+    private lateinit var btnHigher: Button
+    private lateinit var btnLower: Button
+    private lateinit var btnMenu: Button
+
+
+    private  val deck = mutableListOf<String>()
+    private val currentCardValue = 0
+    private val score = 0
+    private val cardsLeft = 52
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_game)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+
+        tvScore = findViewById(R.id.tvScore)
+        tvCardsLeft = findViewById(R.id.tvCardsLeft)
+        tvCard = findViewById(R.id.tvCard)
+        btnHigher = findViewById(R.id.btnHigher)
+        btnLower = findViewById(R.id.btnLower)
+        btnMenu = findViewById(R.id.btnMenu)
+
     }
+
 }
